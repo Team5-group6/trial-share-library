8c08a5351a4f906dc822283ae22af105267f2a6b
Saadatou27 <99763273+Saadatou27@users.noreply.github.com>
2023-04-10 12:44:39 -0500

    Update saadaApp.groovy

--- vars/saadaApp.groovy
+++ vars/saadaApp.groovy
@@ -1,1 +1,42 @@
+def call(string repoUrl){
+pipeline{
+  agent any  
+  stages{
+    stage('1-clone'){
+      when{
+        branch 'feature'
+      }
+      steps{
+        echo 'welcom to Etech-Team5-Group6'
+      }
+    }
+    stage('2-parallel-job'){
+      when{
+        branch 'dev'
+      }
+      parallel{
+        stage('1-parallel-sub-Saada-job1'){
+          when{
+            branch 'main'
+          }
+          steps{
+            sh 'lscpu'
+            sh 'systemctl status jenkins'
+          }
+        }
+        stage('2-parallel-job Francis-job2'){
+          steps{
+            sh 'df -h'
+          }
+        }
+        stage('3-parallel-job Nelson-job3'){
+          steps{
+            sh 'df -h'
+          }
+        }
+      }
+    }
+  }
+}
+}
 
